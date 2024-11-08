3e3fab63ec4706679280eca91d3bf09a87f3cf42
MatchaTi <tehmatcha28@gmail.com>
2024-11-08 09:30:21 +0800

    feat: front-end add navbar component

---
+++ components/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar flex flex-column justify-between items-center">
+    <div class="nav-mobile">
+        <img src="assets/icons/logo.png" alt="logo">
+    </div>
+    <div class="nav-options flex flex-column gap-12">
+        <a href="" class="h-12 w-12 flex items-center justify-center rounded-full shadow border bg-orange">
+            <img src="assets/icons/home.svg" alt="home">
+        </a>
+        <a href="" class="h-12 w-12 flex items-center justify-center rounded-full">
+            <img src="assets/icons/search.svg" alt="search">
+        </a>
+        <a href="" class="h-12 w-12 flex items-center justify-center rounded-full">
+            <img src="assets/icons/tag.svg" alt="tag">
+        </a>
+        <a href="" class="h-12 w-12 flex items-center justify-center rounded-full">
+            <img src="assets/icons/profile.svg" alt="profile">
+        </a>
+    </div>
+    <div class="nav-mobile">
+        <a href="" class="h-12 w-12 flex items-center justify-center rounded-full">
+            <img src="assets/icons/logout.svg" alt="logout">
+        </a>
+    </div>
+</nav>
