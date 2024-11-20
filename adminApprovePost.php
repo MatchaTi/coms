95d2bd633fd56f86a18a6fa41f77adadeac952e6
MatchaTi <tehmatcha28@gmail.com>
2024-11-20 15:29:35 +0800

    chore: add next modal for admin approve user and post

--- adminApprovePost.php
+++ adminApprovePost.php
@@ -1,10 +1,11 @@
 <?php
 include 'includes/functions.php';
+include 'components/nextDialog.php';
 
 session_start();
 
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
-    header('Location: login.php');
+  header('Location: login.php');
 }
 
 $status = $_GET['status'];
@@ -12,22 +13,66 @@ $id = $_GET['id'];
 
 
 if ($status == 'approve') {
-    $result = approvePost($id);
-    if ($result['status']) {
-        echo "<script>alert('" . $result['message'] . "')</script>";
-        header('Location: manajemenPost.php');
-    }
+  $result = approvePost($id);
+  if ($result['status']) {
+    echo nextDialog("manajemenPost.php");
+    echo "<script src='js/nextDialog.js'></script>";
+    echo "<script> 
+    next(
+        'Approve', 
+        '" . $result['message'] . "'
+    );
+    </script>";
+  }
 } else if ($status == 'reject') {
-    $result = rejectPost($id);
-    if ($result['status']) {
-        echo "<script>alert('" . $result['message'] . "')</script>";
-        header('Location: manajemenPost.php');
-    }
+  $result = rejectPost($id);
+  if ($result['status']) {
+    echo nextDialog("manajemenPost.php");
+    echo "<script src='js/nextDialog.js'></script>";
+    echo "<script> 
+    next(
+        'Reject', 
+        '" . $result['message'] . "'
+    );
+    </script>";
+  }
 } else if ($status == 'delete') {
-    $result = deletePost($id);
-    if ($result['status']) {
-        echo "<script>alert('" . $result['message'] . "')</script>";
-        header('Location: manajemenPost.php');
-    }
+  $result = deletePost($id);
+  if ($result['status']) {
+    echo nextDialog("manajemenPost.php");
+    echo "<script src='js/nextDialog.js'></script>";
+    echo "<script> 
+    next(
+        'Delete', 
+        '" . $result['message'] . "'
+    );
+    </script>";
+  }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+  <!-- styles -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/components.css">
+  <link rel="stylesheet" href="css/auth.css">
+
+  <!-- favicon -->
+  <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
+
+  <title>Post | COMS</title>
+</head>
+
+<body></body>
+
+</html>
--- adminApproveUser.php
+++ adminApproveUser.php
@@ -1,30 +1,77 @@
 <?php
 include 'includes/functions.php';
+include 'components/nextDialog.php';
+
+session_start();
 
 if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'admin') {
-    header('Location: login.php');
+  header('Location: login.php');
 }
 
 $status = $_GET['status'];
 $nim = $_GET['nim'];
 
 if ($status == 'approve') {
-    $result = updateStatusUser($nim);
-    if ($result['status']) {
-        echo "<script>alert('" . $result['message'] . "')</script>";
-        header('Location: manajemenUser.php');
-    }
+  $result = updateStatusUser($nim);
+  if ($result['status']) {
+    echo nextDialog("manajemenUser.php");
+    echo "<script src='js/nextDialog.js'></script>";
+    echo "<script> 
+    next(
+        'Approve', 
+        '" . $result['message'] . "'
+    );
+    </script>";
+  }
 } else if ($status == 'reject') {
-    $result = rejectStatusUser($nim);
-    if ($result['status']) {
-        echo "<script>alert('" . $result['message'] . "')</script>";
-        header('Location: manajemenUser.php');
-    }
+  $result = rejectStatusUser($nim);
+  if ($result['status']) {
+    echo nextDialog("manajemenUser.php");
+    echo "<script src='js/nextDialog.js'></script>";
+    echo "<script> 
+    next(
+        'Rejected', 
+        '" . $result['message'] . "'
+    );
+    </script>";
+  }
 } else if ($status == 'delete') {
-    $result = deleteUser($nim);
-    if ($result['status']) {
-        echo "<script>alert('" . $result['message'] . "')</script>";
-        header('Location: manajemenUser.php');
-    }
+  $result = deleteUser($nim);
+  if ($result['status']) {
+    echo nextDialog("manajemenUser.php");
+    echo "<script src='js/nextDialog.js'></script>";
+    echo "<script> 
+    next(
+        'Delete', 
+        '" . $result['message'] . "'
+    );
+    </script>";
+  }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+  <!-- styles -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/components.css">
+  <link rel="stylesheet" href="css/auth.css">
+
+  <!-- favicon -->
+  <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
+
+  <title>User | COMS</title>
+</head>
+
+<body></body>
+
+</html>
